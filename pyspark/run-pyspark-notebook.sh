1a0653c2cbc41ddc39acecfd70f8c3f381f8c26c
bigflood <pyotaesu@gmail.com>
2020-09-23 23:42:16 +0900

    run-pyspark

---
+++ pyspark/run-pyspark-notebook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run --rm --name spark \
+    -v "$(pwd)/data:/data" \
+    -v "$(pwd)/work:/home/jovyan/work" \
+    -e "JUPYTER_ENABLE_LAB=yes" \
+    -p "8888:8888" jupyter/pyspark-notebook
