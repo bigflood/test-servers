9144d4376ed6aecea4c38fa0b6d56bdf3806b6c2
bigflood <pyotaesu@gmail.com>
2020-10-06 21:24:28 +0900

    kafka

---
+++ kafka/dn-kafka.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# wget https://raw.githubusercontent.com/confluentinc/cp-all-in-one/6.0.0-post/cp-all-in-one-community/docker-compose.yml
+
+wget https://raw.githubusercontent.com/confluentinc/cp-all-in-one/6.0.0-post/cp-all-in-one/docker-compose.yml
